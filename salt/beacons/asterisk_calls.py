3368aee4a7af829aa5a13e178c6387cb58f3a2f3
Enrique Zanardi <ezanardi@atlantux.com>
2017-02-28 20:29:43 +0000

    Monitor active channels and calls on Asterisk

---
+++ salt/beacons/asterisk_calls.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+'''
+Beacon to monitor active channels and calls on an Asterisk PBX.
+'''
+
+# Import Python libs
+from __future__ import absolute_import
+import logging
+import re
+
+# Salt libs
+import salt.utils
+
+log = logging.getLogger(__name__)
+
+__virtualname__ = 'asterisk_calls'
+
+CHANNELS = 'channels'
+CALLS = 'calls'
+PROBES = [CHANNELS, CALLS]
+CHANNELS_PATTERN = re.compile("(\d+) active channels?$")
+CALLS_PATTERN = re.compile("(\d+) active calls?$")
+CALLS_WITH_MAXCALLS_PATTERN = re.compile("(\d+) of (\d+) max active calls?")
+
+
+def __virtual__():
+    which_result = salt.utils.which('asterisk')
+    if which_result is None:
+        return False
+    else:
+        return __virtualname__
+
+
+def __validate__(config):
+    '''
+    Validate the beacon configuration
+    '''
+    # Temporary fix for https://github.com/saltstack/salt/issues/38121
+    tmpconfig = {}
+    map(tmpconfig.update, config)
+    config = tmpconfig
+
+    if not isinstance(config, dict):
+        return False, ('Configuration for asterisk_calls beacon '
+                       'must be a dictionary.')
+    for key, value in config.items():
+        if key not in PROBES:
+            return False, ('Invalid name "{0}" for asterisk_calls probe. '
+                           'Must be one of {1}'.format(key, PROBES))
+        if not isinstance(value, list):
+            return False, ('Invalid value "{0}" for asterisk_calls probe {1}. '
+                           'Must be a [min, max] list.'.format(value, key))
+        if len(value) != 2:
+            return False, ('Invalid value "{0}" for asterisk_calls probe {1}. '
+                           'Must be a [min, max] list.'.format(value, key))
+    return True, 'Valid beacon configuration'
+
+
+def beacon(config):
+    '''
+    Monitor active channels and calls on an Asterisk PBX running on the minion.
+
+    Define a range for each probe and emit a beacon when the value is
+    out of range.
+
+    .. code-block:: yaml
+
+        asterisk_calls:
+          channels: [0, 50]
+          calls: [0, 25]
+
+    '''
+    log.trace('asterisk_calls beacon starting')
+    ret = []
+
+    _validate = __validate__(config)
+    if not _validate[0]:
+        return ret
+
+    # Temporary fix for https://github.com/saltstack/salt/issues/38121
+    tmpconfig = {}
+    map(tmpconfig.update, config)
+    config = tmpconfig
+
+    if len(config) == 0:    # Nothing to probe
+        return ret
+
+    # Read values from asterisk
+    out = __salt__['cmd.run']('/usr/sbin/asterisk -x "core show channels"')
+
+    for line in out.split('\n'):
+        # Is an "active channels" line?
+        match = CHANNELS_PATTERN.match(line)
+        if match:
+            channels = match.group(1)
+            if CHANNELS in config:  # probe active channels
+                channels_min, channels_max = config[CHANNELS]
+                if not channels_min <= channels <= channels_max:
+                    ret.append({
+                        'tag': 'active_channels',
+                        'channels': channels
+                    })
+            continue    # Process next line of output
+        # Is an "active calls" line? (2 patterns, without and with maxcalls)
+        match = CALLS_PATTERN.match(line)
+        if not match:
+            match = CALLS_WITH_MAXCALLS_PATTERN.match(line)
+        if match:
+            calls = match.group(1)
+            if CALLS in config:  # probe active calls
+                calls_min, calls_max = config[CALLS]
+                if not calls_min <= calls <= calls_max:
+                    ret.append({
+                        'tag': 'active_calls',
+                        'calls': calls
+                    })
+
+    return ret
